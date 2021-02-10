e7f3f6f3a178b08d2ac8fb0f513f9597bdc53d34
gwy15 <gwy15thu@gmail.com>
2021-02-10 11:44:52 +0800

    works now

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 #![allow(unused)]
 
+use std::marker::PhantomData;
+
 struct App {
     // handlers: Vec<Box<dyn Handler>>,
     services: Vec<Box<dyn Service>>,
@@ -16,30 +18,75 @@ impl App {
         self.services.push(Box::new(ServiceWrapper::new(f)));
         self
     }
+    pub fn dispatch(&self, req: String) {
+        for service in self.services.iter() {
+            service.handle_request(&req);
+        }
+    }
 }
 
 /// 要求 T 可解析
-trait FromRequest {}
-impl FromRequest for () {}
-impl FromRequest for String {}
-impl FromRequest for u32 {}
-impl FromRequest for u64 {}
-impl<T1> FromRequest for (T1,) where T1: FromRequest {}
+trait FromRequest {
+    fn from_request(req: &str) -> Self;
+}
+impl FromRequest for () {
+    fn from_request(req: &str) -> Self {
+        ()
+    }
+}
+impl FromRequest for String {
+    fn from_request(req: &str) -> Self {
+        req.to_string()
+    }
+}
+impl FromRequest for u32 {
+    fn from_request(req: &str) -> Self {
+        req.parse().unwrap()
+    }
+}
+impl FromRequest for u64 {
+    fn from_request(req: &str) -> Self {
+        req.parse().unwrap()
+    }
+}
+impl<T1> FromRequest for (T1,)
+where
+    T1: FromRequest,
+{
+    fn from_request(req: &str) -> Self {
+        (T1::from_request(req),)
+    }
+}
 impl<T1, T2> FromRequest for (T1, T2)
 where
     T1: FromRequest,
     T2: FromRequest,
 {
+    fn from_request(req: &str) -> Self {
+        (T1::from_request(req), T2::from_request(req))
+    }
 }
 
 /// 设置 T 为 Handler 接受的类型
-trait Handler<T>: Clone + 'static {}
-impl<F> Handler<()> for F where F: Fn() -> () + Clone + 'static {}
+trait Handler<T>: Clone + 'static {
+    fn call(&self, param: T);
+}
+impl<F> Handler<()> for F
+where
+    F: Fn() -> () + Clone + 'static,
+{
+    fn call(&self, param: ()) {
+        (self)()
+    }
+}
 impl<F, T> Handler<(T,)> for F
 where
     F: Fn(T) -> () + Clone + 'static,
     T: FromRequest,
 {
+    fn call(&self, param: (T,)) {
+        (self)(param.0)
+    }
 }
 impl<F, T1, T2> Handler<(T1, T2)> for F
 where
@@ -47,30 +94,53 @@ where
     T1: FromRequest,
     T2: FromRequest,
 {
-}
-
-trait Service {}
-
-struct ServiceWrapper<F> {
-    f: F,
-}
-impl<F> ServiceWrapper<F> {
-    pub fn new<T>(f: F) -> Self
-    where
-        F: Handler<T>,
-    {
-        Self { f }
+    fn call(&self, param: (T1, T2)) {
+        (self)(param.0, param.1)
     }
 }
-impl<T> Service for ServiceWrapper<T> {}
+
+trait Service {
+    fn handle_request(&self, req: &str);
+}
+
+struct ServiceWrapper<F, T> {
+    f: F,
+    _t: PhantomData<T>,
+}
+impl<F, T> ServiceWrapper<F, T> {
+    pub fn new(f: F) -> Self
+    where
+        F: Handler<T>,
+        T: FromRequest,
+    {
+        Self { f, _t: PhantomData }
+    }
+}
+impl<F, T> Service for ServiceWrapper<F, T>
+where
+    F: Handler<T>,
+    T: FromRequest,
+{
+    fn handle_request(&self, req: &str) {
+        let params = T::from_request(req);
+        self.f.call(params);
+    }
+}
 
 #[test]
 fn test_add_handlers() {
-    fn none() {}
+    fn none() {
+        println!("print from none");
+    }
 
-    fn one(s: String) {}
+    fn one(s: String) {
+        println!("print from one: s = {}", s);
+    }
 
-    fn two(n1: u32, n2: u64) {}
+    fn two(n1: u32, n2: u64) {
+        println!("print from two: n1 = {}, n2 = {}", n1, n2);
+    }
 
-    App::new().handler(none).handler(one).handler(two);
+    let app = App::new().handler(none).handler(one).handler(two);
+    app.dispatch("1234".to_string());
 }
